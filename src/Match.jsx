b7b532c6eb030148765464f15aececd37692c4b7
Anand Sharma <anand2132>
2020-10-02 22:39:43 +0530

    Slot game Changes

--- src/Match.jsx
+++ src/Match.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Match = (props) => {
-    console.log("calling...");
+    //console.log("calling...");
     let {x,y,z} = props;
-    if (props.x == props.y && props.y == props.z && props.z == props.x){
-        console.log("success" + props.x,props.y,props.z);
+    if (props.x === props.y && props.y === props.z && props.z === props.x){
+        //console.log("success" + props.x,props.y,props.z);
         return <p>{props.x}{props.y}{props.z}<br />This is Matching</p>;
         
     }else{
-        console.log("failed" + props.x,props.y,props.z);
+        //console.log("failed" + props.x,props.y,props.z);
         return <p>{props.x}{props.y}{props.z}<br />This is Not Matching</p>;
     };
 }
--- src/SlotMachine.jsx
+++ src/SlotMachine.jsx
@@ -4,7 +4,8 @@ import Match from './Match';
 
 const emoji = ["😀", "😂", "😎", "🤣", "🤗", "🤩", "😆"];
 let x,y,z;
-
+let r;
+let hi_score = 300;
 function Fun(v=""){
     for (var i=0;i<3;i++){
         let random = Math.floor(Math.random() * emoji.length);
@@ -23,9 +24,12 @@ function Fun(v=""){
 }
 let count = 0;
 let score = 0;
-let maxCount = 50;
+let maxCount = 100;
 let hit = 0;
-
+let flag = 0;
+let res1,res2,res3;
+let pass2,pass3 = 0;
+let result;
 function myFunction() {
     var x = document.getElementById("msg");
     if (x.style.display === "none") {
@@ -36,47 +40,137 @@ function myFunction() {
   } 
 
 
+
+
 function call(){
+    //console.log(flag);
+    document.getElementById("hi_score").innerHTML = hi_score;
+document.getElementById("count").innerHTML = count;
+document.getElementById("score").innerHTML = score;
+    
     if(count == 0){
-        document.getElementById("btn").innerHTML = "HIT";
+        document.getElementById("btn").innerHTML = "PLAY";
         myFunction();
     }
-    let res = <Match x={Fun(1)} y={Fun(1)} z={Fun(1)} />
+
+    if(flag == 0){
+        res1 = <Match x={Fun(1)} y={Fun(1)} z={Fun(1)} />;
+    }else if(flag == 1){
+        res2 = <Match x={Fun(1)} y={Fun(1)} z={Fun(1)} />;
+    }else{
+        res3 = <Match x={Fun(1)} y={Fun(1)} z={Fun(1)} />;
+    }
+    
+
     count += 1;
     hit += 1;
     document.getElementById("count").innerHTML = count;
     document.getElementById("score").innerHTML = score;
     
+
     if(count > maxCount){
         count = 0;
-        document.getElementById("btn").innerHTML = "Play Again";
+        stop_btn = document.getElementById("stop_btn");
+        run_btn = document.getElementById("btn");
+        if (stop_btn.style.display === "block") {
+            stop_btn.style.display = "none";
+            run_btn.style.display = "block";
+            run_btn.style.margin = "10px auto";
+            run_btn.innerHTML = "Play Again";
+        }
+        //document.getElementById("btn").innerHTML = "Play Again";
         document.getElementById("count").innerHTML = count;
         myFunction();
         hit -= 1
-        var result = document.getElementById("msg").innerHTML = "😭 You Lose the Game! 😭<br />Your Total Score is : " + score + "<br />Your Total Hits : " + hit;
+        if (score > hi_score){
+            result = document.getElementById("msg").innerHTML = "😎 Congratulation! You Won the Game 😎<br />Your Total Score is : " + score + "<br />Your Total Hits : " + hit;
+        }else{
+            var result = document.getElementById("msg").innerHTML = "😭 You Lose the Game! 😭<br />Your Total Score is : " + score + "<br />Your Total Hits : " + hit;
+        }
+        
         score = 0;
         hit = 0;
+        flag = 0;
+        clearInterval(r);
+
         return result;
     }
-    if (res.props.x == res.props.y && res.props.y == res.props.z && res.props.z == res.props.x){
-        count = 0;
-        score += 100;
-        document.getElementById("btn").innerHTML = "Continue";
-        return (document.getElementById("f1").innerHTML = res.props.x + " " + res.props.y + " " + res.props.z + "<br />" + "This is Matching",
-        document.getElementById("f2").innerHTML = res.props.x + " " + res.props.y + " " + res.props.z + "<br />" + "This is Matching",
-        document.getElementById("f3").innerHTML = res.props.x + " " + res.props.y + " " + res.props.z + "<br />" + "This is Matching",
-        myFunction(),
-        document.getElementById("msg").innerHTML = "😎 Congratulation! You Won the Game 😎"
-        );
-    }else{
-        return (document.getElementById("f1").innerHTML = res.props.x + " " + res.props.y + " " + res.props.z + "<br />" + "This is Not Matching",
-        document.getElementById("f2").innerHTML = res.props.x + " " + res.props.y + " " + res.props.z + "<br />" + "This is Not Matching",
-        document.getElementById("f3").innerHTML = res.props.x + " " + res.props.y + " " + res.props.z + "<br />" + "This is Not Matching"
-        );
+    if(flag === 0){
+        if(pass2 === 1){
+            pass2 = 0;
+            document.getElementById("f2").innerHTML = Fun(1) + " " + Fun(1) + " " + Fun(1) + "<br />" + "This is Not Matching"
+            if(pass3 === 1){
+                pass3 = 0;
+                document.getElementById("f3").innerHTML = Fun(1) + " " + Fun(1) + " " + Fun(1) + "<br />" + "This is Not Matching"
+            }
+        }
+        if (res1.props.x === res1.props.y && res1.props.y === res1.props.z && res1.props.z === res1.props.x){
+            flag = 1;
+            return document.getElementById("f1").innerHTML = res1.props.x + " " + res1.props.y + " " + res1.props.z + "<br />" + "This is Matching";
+        }else{
+            return document.getElementById("f1").innerHTML = res1.props.x + " " + res1.props.y + " " + res1.props.z + "<br />" + "This is Not Matching";
+        }
     }
     
+
+    if(flag == 1){
+        if (res2.props.x === res2.props.y && res2.props.y === res2.props.z && res2.props.z === res2.props.x){
+            flag = 2;
+            pass2 = 1;
+            return document.getElementById("f2").innerHTML = res2.props.x + " " + res2.props.y + " " + res2.props.z + "<br />" + "This is Matching";
+        }else{
+            return document.getElementById("f2").innerHTML = res2.props.x + " " + res2.props.y + " " + res2.props.z + "<br />" + "This is Not Matching";
+        }
+    }
+
+    
+
+    if(flag == 2){
+        if (res3.props.x === res3.props.y && res3.props.y === res3.props.z && res3.props.z === res3.props.x){
+            flag = 0; 
+            pass3 = 1;
+            count = 0;
+            score += 100;
+            document.getElementById("btn").innerHTML = "Continue";
+            
+            return (document.getElementById("f3").innerHTML = res3.props.x + " " + res3.props.y + " " + res3.props.z + "<br />" + "This is Matching",
+            myFunction(),
+            document.getElementById("msg").innerHTML = "😎 Congratulation! You Won the Game 😎"
+            );
+        }else{
+            return document.getElementById("f3").innerHTML = res3.props.x + " " + res3.props.y + " " + res3.props.z + "<br />" + "This is Not Matching";
+        }
+    }
+
 }
 
+var stop_btn,run_btn;
+function RunGame(t='') {
+    
+    r = setInterval(call, 180);
+    stop_btn = document.getElementById("stop_btn");
+    run_btn = document.getElementById("btn");
+    if (stop_btn.style.display === "none") {
+        stop_btn.style.display = "block";
+        run_btn.style.display = "none";
+    } else {
+        stop_btn.style.display = "none";
+        run_btn.style.display = "block";
+    }
+}
+
+function StopGame(){
+    clearInterval(r);
+    stop_btn = document.getElementById("stop_btn");
+    run_btn = document.getElementById("btn");
+    if (stop_btn.style.display === "block") {
+        stop_btn.style.display = "none";
+        run_btn.style.display = "block";
+    } else {
+        stop_btn.style.display = "block";
+        run_btn.style.display = "none";
+    }
+}
 
 
 
@@ -87,11 +181,23 @@ const SlotM = () => {
         <h1 className="para1">🎰 Welcome to Slot Machine 🎰</h1>
         <div className="slot_machine">
             <p className="para1" style={ {backgroundColor:"#fff"} } id="msg"></p>
-            <h5 className="heading2" style={ {display:"block-ruby"} }><div className="turn">Your Turn : <p className="para1" id="count" style={ {margin:"20px 55px 0px 50px" }}></p></div><div className="score">Your Score : <p className="para1"  id="score"></p></div></h5>
+            <h5 className="heading2" style={ {display:"block-ruby"} }>
+                <div className="turn">
+                    Your Turn : <p className="para1" id="count">{count}</p>
+                </div>
+                <div className="score">
+                    Your Score : <p className="para1"  id="score">{score}</p>
+                </div>
+                <div className="hi_score">
+                    Hi-Score : <p className="para1"  id="hi_score">{hi_score}</p>
+                </div>
+            </h5>
+
             <h1 className="slot_inner" id="f1"><Match x={Fun(1)} y={Fun(1)} z={Fun(1)} /></h1>
             <h1 className="slot_inner" id="f2"><Match x={Fun(1)} y={Fun(1)} z={Fun(1)} /></h1>
             <h1 className="slot_inner" id="f3"><Match x={Fun(1)} y={Fun(1)} z={Fun(1)} /></h1>
-            <button className="btn btn-success slot_btn" id="btn" onClick={call}>HIT</button>
+            <button className="btn btn-success slot_btn" id="btn" onClick={() => RunGame('')}>PLAY</button>
+            <button className="btn btn-success slot_btn" id="stop_btn" style={ {display:"none"} } onClick={StopGame}>STOP</button>
         </div>
         </>
     );
